bcb2b25844d41015216ee72b1bca6675ae12e186
ArjanKalsi <109098789+ArjanKalsi@users.noreply.github.com>
2023-03-20 18:05:19 +0000

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class Feed extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Feed</Text>
+      </View>
+    );
+  }
+}
